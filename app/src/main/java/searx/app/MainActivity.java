aa6881b66fc65e82c4c625b2e4a41edb40e65d9d
youtube-dl <72506029+HSDChannel@users.noreply.github.com>
2021-09-24 01:36:04 +0700

    Add files via upload

--- app/src/main/java/searx/app/MainActivity.java
+++ app/src/main/java/searx/app/MainActivity.java
@@ -3,15 +3,21 @@ package searx.app;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.TargetApi;
+import android.app.Activity;
+import android.content.DialogInterface;
 import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.Button;
 
 import java.lang.annotation.Target;
 
 public class MainActivity extends AppCompatActivity {
+    Button button3;
+    Button button4;
     private WebView Web;
 
     @Override
@@ -23,6 +29,22 @@ public class MainActivity extends AppCompatActivity {
         ws.setJavaScriptEnabled(true);
         Web.loadUrl("https://searx.bar/");
         Web.setWebViewClient(new WebViewClient());
+
+        button3 = (Button) findViewById(R.id.button3);
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Web.loadUrl("https://searx.bar/");
+            }
+        });
+
+        button4 = (Button) findViewById(R.id.button4);
+        button4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
     }
 
     @Override
